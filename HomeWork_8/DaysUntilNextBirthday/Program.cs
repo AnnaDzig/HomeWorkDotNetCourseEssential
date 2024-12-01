322cdf9b3ad4ab4463993043672fbf0d4f48b6dd
Anna <77171982+AnnaDzig@users.noreply.github.com>
2024-12-01 16:39:31 +0100

    Add files via upload

---
+++ HomeWork_8/DaysUntilNextBirthday/Program.cs
@@ -0,0 +1,37 @@
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Введіть вашу дату народження (в форматі день місяць рік ставлячи пробіл після кожного значення  ): ");
+            string input = Console.ReadLine();
+
+            DateTime birthDate;
+            if (DateTime.TryParse(input, out birthDate))
+            {
+                DateTime currentDate = DateTime.Today;
+
+                DateTime nextBirthday = new DateTime(currentDate.Year, birthDate.Month, birthDate.Day);
+
+                if (nextBirthday < currentDate)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+
+                int daysUntilBirthday = (nextBirthday - currentDate).Days;
+
+                Console.WriteLine($"До наступного дня народження залишилось {daysUntilBirthday} днів.");
+            }
+            else
+            {
+                Console.WriteLine("Введена дата некоректна. Будь ласка, введіть дату у правильному форматі.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+
