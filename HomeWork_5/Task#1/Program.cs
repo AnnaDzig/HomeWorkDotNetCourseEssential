537d781d7fed406e03897cbc958017e983d3a2ca
Anna <77171982+AnnaDzig@users.noreply.github.com>
2024-12-01 15:09:30 +0100

    Add files via upload

---
+++ HomeWork_5/Task#1/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть кількість елементів масиву (N): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] array = new int[n];
+
+        Console.WriteLine("Введіть елементи масиву:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Елемент {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = array[0];
+        int min = array[0];
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] > max)
+                max = array[i];
+
+            if (array[i] < min)
+                min = array[i];
+
+            sum += array[i];
+        }
+
+        double average = (double)sum / n;
+
+        Console.WriteLine("\nНепарні значення:");
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] % 2 != 0)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        // Виведення результатів
+
+        Console.WriteLine($"\n\nНайбільше значення: {max}");
+        Console.WriteLine($"Найменше значення: {min}");
+        Console.WriteLine($"Сума елементів: {sum}");
+        Console.WriteLine($"Середнє арифметичне: {average:F2}");
+
+
+        Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
+        Console.ReadKey();
+    }
+}
