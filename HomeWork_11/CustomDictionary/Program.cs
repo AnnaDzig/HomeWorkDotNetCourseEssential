81ed314c046034ff4f1227e08d4f526ae0926568
AnnaDzig <adzyhota@protonmail.com>
2024-12-11 23:24:47 +0100

    add home work for lesson 11

---
+++ HomeWork_11/CustomDictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+class Dictionary<TKey, TValue>
+{
+    private readonly List<TKey> keys = new List<TKey>();
+    private readonly List<TValue> values = new List<TValue>();
+
+    public void Add(TKey key, TValue value)
+    {
+        if (keys.Contains(key))
+       
+            throw new ArgumentException("Такий ключ уже існує.");
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public TValue this[TKey key]
+    {
+
+        get
+        {
+            int index = keys.IndexOf(key);
+            if (index == -1)
+                throw new ArgumentException("Ключ не знайдено.");
+            return values[index];
+        }
+    }
+
+    public int Count => keys.Count;
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        var dictionary = new Dictionary<string, int>();
+
+        dictionary.Add("Apple", 10);
+        dictionary.Add("Orange", 15);
+        dictionary.Add("Kiwi", 9);
+
+
+        Console.WriteLine($"Кількість яблук: {dictionary["Apple"]} ");
+        Console.WriteLine($"Кількість апельсин:{dictionary["Orange"]}");
+        Console.WriteLine($"Кількість ківі:{dictionary["Kiwi"]}");
+
+        Console.WriteLine($"Загальна кількість пар у словнику: {dictionary.Count}");
+
+        try
+        {
+            dictionary.Add("Kiwi", 50);
+        }
+        catch (ArgumentException ex)
+        { Console.WriteLine($"Error:{ex.Message}");
+        }
+    }
+}
---
+++ HomeWork_11/Task#6_ArrayList/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections; 
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        ArrayList arrayList = new ArrayList();
+
+        arrayList.Add(42);               
+        arrayList.Add(3.14);             
+        arrayList.Add("Hello, world!");  
+        arrayList.Add(DateTime.Now);     
+        arrayList.Add(new object());    
+
+        Console.WriteLine("Елементи в ArrayList:");
+        for (int i = 0; i < arrayList.Count; i++)
+        {
+            Console.WriteLine($"Index {i}: {arrayList[i]}");
+        }
+
+        try
+        {
+            int value = (int)arrayList[0]; 
+            Console.WriteLine($"\nКоректне приведення: {value}");
+
+            value = (int)arrayList[1]; 
+        }
+        catch (InvalidCastException ex)
+        {
+            Console.WriteLine($"\nПомилка: {ex.Message}");
+        }
+    }
+}
+
+//  Коли ми запустили код, то зіткнулися з тим, що `ArrayList` дозволяє додавати елементи різних типів,
+//  але при доступі до них потрібно явно приводити їх до потрібного типу. Якщо тип елемента не
+//  відповідає очікуваному, виникає виняток `InvalidCastException`. Це трапляється через те, що `ArrayList`
+//  не підтримує перевірку типів на етапі компіляції, що робить код менш надійним і схильним до помилок
+//  під час виконання.
