366820ad8233f4a4225dedb0083bce978d629a98
Anna <77171982+AnnaDzig@users.noreply.github.com>
2025-01-20 10:00:51 +0100

    add Home work for the lesson 17

---
+++ HomeWork_17_AnonymousTypes_LINQ/DynamicCalculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCalculator
+{
+    internal class Calculator
+    {
+        public dynamic Add(dynamic a, dynamic b)
+        {
+            return a + b;
+        }
+
+        public dynamic Subtract(dynamic a, dynamic b)
+        {
+            return a - b;
+        }
+
+        public dynamic Multiply(dynamic a, dynamic b)
+        {
+            return a * b;
+        }
+
+        public dynamic Divide(dynamic a, dynamic b)
+        {
+            if (b == 0)
+            {
+                return "Error: Division by zero";
+            }
+            return a / b;
+        }
+    }
+}
+
